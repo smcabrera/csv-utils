9eebfc89035f189ddc583c8fc65bd0012bbda76d
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-08-24 22:59:52 +0200

    meeting changes

--- append.rb
+++ append.rb
@@ -1,22 +1,7 @@
+#!/usr/bin/env ruby
 
-require 'csv'
-require './merge_helper'
-require 'pry'
+require_relative './merge-helper'
 
-# We don't need to do the gymnastics we were doing earlier by extracting a list from a list and then creating a list...this is already an array!
+csv_table = MergeHelpers.append_csv_files(ARGV)
 
-# So the format of this script is as follows--any number of filenames with the last filename being the name of the file that is to be output
-
-output_filename = ARGV.pop # The last argument is the title
-
-MergeHelpers.output_table output_filename, MergeHelpers.append_csv_files(ARGV) # output_table requires a name for the file and an array of csv files to append
-
-binding.pry
-
-
-
-
-
-
-
-
+puts csv_table.to_s
